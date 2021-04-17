40690737a0b1d4fbe8bd8d056edc63166840327f
Hridoy <hridoyswe205@gmail.com>
2021-04-17 13:53:34 +0600

    Function

---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/FunctionEx-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FunctionEx_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> xs = OddSequence(76, 7);
+            Console.WriteLine("Retrived enumerat0");
+
+            foreach(var x in xs)
+            {
+                Console.Write($"{x}");
+            }
+        }
+
+        public static IEnumerable<int> OddSequence (int start, int end)
+        {
+            if (start < 0 || start > 99)
+                throw new ArgumentOutOfRangeException(nameof(start), "Start must be between 0 and 99");
+            if (end >= 100)
+                throw new ArgumentOutOfRangeException(nameof(end), "end must be less then or equal to 100");
+            if (start >= end)
+                throw new ArgumentException("start must be less then to end");
+
+            for(int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                    yield return i;
+            }
+        }
+    }
+}

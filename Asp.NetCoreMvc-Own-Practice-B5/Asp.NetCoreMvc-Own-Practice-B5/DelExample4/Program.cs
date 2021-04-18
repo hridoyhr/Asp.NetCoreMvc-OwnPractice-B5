5c6cf3dfbd553e0e2e69b4e9f93c316be0aaf9a5
Hridoy <hridoyswe205@gmail.com>
2021-04-18 23:00:20 +0600

    Delegate

---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/DelExample4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DelExample4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var designerr = new PuzzleMaker.Design(StrangeDesing);
+            Print(designerr);
+        }
+
+        static void StrangeDesing(int x, int y)
+        {
+            Console.WriteLine($"Printing from static method: {x}, {y}");
+        }
+
+        static void Print(PuzzleMaker.Design designer)
+        {
+            Console.WriteLine("Welcome to printing: ");
+            designer(5, 7);
+        }
+    }
+}

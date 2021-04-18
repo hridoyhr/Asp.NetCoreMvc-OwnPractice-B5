e4a4f23b9da95460632c27152e42d40f9480704b
Hridoy <hridoyswe205@gmail.com>
2021-04-18 15:35:30 +0600

    Delegate Example

---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/DelExample1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DelExample1
+{
+    class Program
+    {
+        delegate void Perform(string text);
+
+        static void Main(string[] args)
+        {
+
+            var text = "my message";
+            ProcessText(text);
+
+        }
+
+        static void PrintMethod(string textPoint)
+        {
+            Console.WriteLine($"--{textPoint}---");
+        }
+        
+
+        static void ProcessText(string text)
+        {
+            if(!string.IsNullOrWhiteSpace(text))
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/EventExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EventExample
+{
+    class Program
+    {
+        public delegate void Message(string message);
+
+        static void Main(string[] args)
+        {
+            Message print = Output;
+            print("Hey I am Here");
+
+        }
+
+        public static void Output(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        
+    }
+}

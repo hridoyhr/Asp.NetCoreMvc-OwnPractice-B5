976df1b42de18027e0fc1969bfc37c49ae336c92
Hridoy <hridoyswe205@gmail.com>
2021-04-22 19:45:46 +0600

    Array of Delegate

---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/ArrayOfDeli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArrayOfDeli
+{
+    class Program
+    {
+        delegate void DelOp(int x, int y);
+
+        static void Main(string[] args)
+        {
+            //Delegate Instantiation
+            DelOp[] obj =
+            {
+                new DelOp(Operation.Add),
+
+                new DelOp(Operation.Multiple)
+            };
+
+            for(int i = 0; i < obj.Length; i++)
+            {
+                obj[i](2, 5);
+                obj[i](20, 5);
+                obj[i](5, 5);
+                obj[i](25, 5);
+            }
+
+        }
+    }
+}

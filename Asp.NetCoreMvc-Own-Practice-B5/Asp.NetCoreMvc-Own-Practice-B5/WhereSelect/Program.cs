f7bb4d542cd2a0f2f6da895009e8100084d9574d
Hridoy <hridoyswe205@gmail.com>
2021-04-23 12:49:35 +0600

    where clause in LINQ

---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/WhereSelect/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace WhereSelect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[] { 97, 60, 80, 220, 104};
+
+            var scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+
+            foreach (var print in scoreQuery)
+            {
+                Console.WriteLine(print + " ");
+            }
+        }
+    }
+}

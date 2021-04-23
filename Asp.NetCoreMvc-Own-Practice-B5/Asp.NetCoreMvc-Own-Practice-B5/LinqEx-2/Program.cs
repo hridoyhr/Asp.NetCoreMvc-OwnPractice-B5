8d7285b1bb1138898984270e6f1fd923f39c63d7
Hridoy <hridoyswe205@gmail.com>
2021-04-23 16:04:22 +0600

    Order by key

--- Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/LinqEx-2/Program.cs
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/LinqEx-2/Program.cs
@@ -51,15 +51,47 @@ namespace LinqEx_2
             #endregion
 
             #region Modify the Query "OrderBY"
-            var studentQuery3 =
-                from student in students
-                orderby student.Last descending
-                select student;
+            //var studentQuery3 =
+            //    from student in students
+            //    orderby student.Last descending
+            //    select student;
 
-            foreach(Student student1 in studentQuery3)
+            //foreach(Student student1 in studentQuery3)
+            //{
+            //    //Console.WriteLine("{0}", student1.Last);
+            //    Console.WriteLine("{0} , {1}, {2}", student1.Last, student1.First, student1.Scores[0]);
+            //}
+            #endregion
+
+            #region Group Query
+            //var studentQuery4 =
+            //    from student in students
+            //    group student by student.Last[0];
+
+            //foreach(var studentGroup in studentQuery4)
+            //{
+            //    Console.WriteLine(studentGroup.Key);
+            //    foreach(Student student in studentGroup)
+            //    {
+            //        Console.WriteLine("{0},{1}", student.Last, student.First);
+            //    }
+            //}
+            #endregion
+
+            #region Order the groups by their key value
+            var studentQuery5 =
+                from student in students
+                group student by student.Last[0] into studentGroup
+                orderby studentGroup.Key
+                select studentGroup;
+
+            foreach(var groupOFStudent in studentQuery5)
             {
-                //Console.WriteLine("{0}", student1.Last);
-                Console.WriteLine("{0} , {1}, {2}", student1.Last, student1.First, student1.Scores[0]);
+                Console.WriteLine(groupOFStudent.Key);
+                foreach(var student in groupOFStudent)
+                {
+                    Console.WriteLine("{0} {1}", student.Last, student.First);
+                }
             }
             #endregion
         }

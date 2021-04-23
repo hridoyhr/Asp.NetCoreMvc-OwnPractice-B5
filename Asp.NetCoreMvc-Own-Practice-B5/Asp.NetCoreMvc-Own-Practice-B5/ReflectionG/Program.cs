75aca1fb3419f9241ee77acc7b9351ba2f48fe02
Hridoy <hridoyswe205@gmail.com>
2021-04-23 22:28:01 +0600

    modify

---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/ReflectionG/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ReflectionG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetDetails<Student>();
+            
+        }
+        public static void GetDetails<T>() where T : class
+        {
+            List<Student> studentList = new List<Student>();
+
+            
+        }
+    }
+}
---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/ReflectionG/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionG
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+        public Info info { get; set; }
+    }
+
+    public class Info
+    {
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}

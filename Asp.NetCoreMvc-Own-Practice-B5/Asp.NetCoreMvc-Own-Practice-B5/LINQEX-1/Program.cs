cba0fed0c62414e40cfb55348f333ccf50fb5187
Hridoy <hridoyswe205@gmail.com>
2021-04-23 12:35:23 +0600

    Thread and Linq

---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/LINQEX-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQEX_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "hello", "beautifull", "like", "some"};
+
+            var shortWord = from word in words
+                            where word.Length <= 5
+                            select word;
+
+            foreach(var word in shortWord)
+                Console.WriteLine(word);
+        }
+    }
+}

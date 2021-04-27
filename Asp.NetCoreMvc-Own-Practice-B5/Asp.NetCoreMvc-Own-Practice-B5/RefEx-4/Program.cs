a28f61b50118134bc01ee749162f5ea9e0ccad7d
hridoyhr <hridoyswe205@gmail.com>
2021-04-27 14:26:33 +0600

    Reflection

--- Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/RefEx-4/Program.cs
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/RefEx-4/Program.cs
@@ -29,6 +29,7 @@ namespace RefEx_4
 
                 item.PrintMe();
             }
+            
         }
     }
 }
---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/RefEx4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace RefEx4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+
+            foreach(var type in types)
+            {
+                if (type.GetInterfaces().Any(x => x.Name == "IStudent"))
+                {
+                    Console.WriteLine(type.GetProperties());
+                }
+            }
+        }
+    }
+}

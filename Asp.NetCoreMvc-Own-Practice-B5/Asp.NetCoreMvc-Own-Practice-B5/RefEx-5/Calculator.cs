9510604cfa7e51a90b7a4281547828189c342eaa
hridoyhr <hridoyswe205@gmail.com>
2021-04-27 14:36:37 +0600

    Reflection

---
+++ Asp.NetCoreMvc-Own-Practice-B5/Asp.NetCoreMvc-Own-Practice-B5/RefEx-5/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefEx_5
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+        private double _number;
+        public double Number { get; set; }
+        public void  Clear()
+        {
+
+        }
+        private void DoClear()
+        {
+
+        }
+        public double Add(double number)
+        {
+            return number;
+        }
+    }
+}

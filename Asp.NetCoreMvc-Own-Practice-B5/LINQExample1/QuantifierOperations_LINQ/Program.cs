89ccc273ebddbfe4e5c8165ccef41e2902bfed7b
hridoyhr <hridoyswe205@gmail.com>
2021-04-28 19:38:13 +0600

    Any method in Quantifier Operation

--- Asp.NetCoreMvc-Own-Practice-B5/LINQExample1/QuantifierOperations_LINQ/Program.cs
+++ Asp.NetCoreMvc-Own-Practice-B5/LINQExample1/QuantifierOperations_LINQ/Program.cs
@@ -10,17 +10,26 @@ namespace QuantifierOperations_LINQ
         {
             new Market { Name = "Emily", Items = new string[]{ "kiwi", "cherry", "banana"} },
             new Market { Name = "Kim's", Items = new string[]{ "mango", "melon", "olive"} },
-            new Market { Name = "Emily", Items = new string[]{ "kiwi", "apple", "orange"} }
+            new Market { Name = "Hridoy", Items = new string[]{ "kiwi", "apple", "orange"} }
         };
         static void Main(string[] args)
         {
             //Determine which market have all fruits names length equal = 5
             #region All(method)
-            var names = from market in markets
-                        where market.Items.All(item => item.Length == 5)
-                        select market.Name;
+            //var names = from market in markets
+            //            where market.Items.All(item => item.Length == 5)
+            //            select market.Name;
 
-            foreach(string name in names)
+            //foreach(string name in names)
+            //    Console.WriteLine(name);
+            #endregion
+
+            #region Any Method
+            var names = from market in markets
+                       where market.Items.Any(item => item.StartsWith("o"))
+                       select market.Name;
+
+            foreach(var name in names)
                 Console.WriteLine(name);
             #endregion
         }

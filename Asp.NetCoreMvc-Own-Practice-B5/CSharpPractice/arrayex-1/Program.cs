3d983f652be5f143312b1dcc19c5b134458e9e38
hridoyhr <hridoyswe205@gmail.com>
2021-04-30 00:27:08 +0600

    nothing to change

---
+++ Asp.NetCoreMvc-Own-Practice-B5/CSharpPractice/arrayex-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arrayex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

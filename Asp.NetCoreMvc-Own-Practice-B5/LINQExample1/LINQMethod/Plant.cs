e2b4ed601e1c810bfa395b9299f3ca452c5229bd
hridoyhr <hridoyswe205@gmail.com>
2021-04-30 11:09:59 +0600

    develop

---
+++ Asp.NetCoreMvc-Own-Practice-B5/LINQExample1/LINQMethod/Plant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQMethod
+{
+    public class Plant
+    {
+        public string Name { get; set; }
+    }
+
+    class CategoryPlant : Plant
+    {
+        public string TrapType { get; set; }
+    }
+
+    
+}
---
+++ Asp.NetCoreMvc-Own-Practice-B5/LINQExample1/LINQMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQMethod
+{
+    class Program
+    {
+        static Plant[] plants = new Plant[]
+            {
+                new CategoryPlant { Name = "Vanus Fly Trap", TrapType = "Snap Trap"},
+                new CategoryPlant { Name = "Pitcher Plant", TrapType = "Pitfall tarap"},
+                new CategoryPlant { Name = "Sundew", TrapType = "Flypaper Trap"},
+                new CategoryPlant { Name = "Water Wheel Plant", TrapType = "Snap Trap"}
+            };
+        static void Main(string[] args)
+        {
+            var query = from CategoryPlant plant in plants
+                        where plant.TrapType == "Snap Trap"
+                        select plant;
+
+            foreach(Plant plant in query)
+                Console.WriteLine(plant.Name);
+
+        }
+    }
+}

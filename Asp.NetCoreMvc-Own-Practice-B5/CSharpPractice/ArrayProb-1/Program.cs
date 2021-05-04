da93ded4cbf14ff24371c62b27ebf405e3edbe4f
hridoyhr <hridoyswe205@gmail.com>
2021-05-05 00:38:05 +0600

    C# practice

---
+++ Asp.NetCoreMvc-Own-Practice-B5/CSharpPractice/ArrayProb-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayProb_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];
+
+            for(int i = 0; i < 6; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            int a = 0;
+            for(int i =0; i < 6; i++)
+            {
+                a += 10;
+                arr[i] = a;
+            }
+
+            foreach (int i in arr)
+                Console.Write(i + " ");
+        }
+    }
+}

4ebf2ea7cc616ef67a45fdf4685e1d64862e5d19
hridoyhr <hridoyswe205@gmail.com>
2021-05-08 13:25:45 +0600

    Dependency Injection

---
+++ Asp.NetCoreMvc-Own-Practice-B5/DependencyEx-1/AccountManagementService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyEx_1
+{
+    public class AccountManagementService
+    {
+        private IDatabaseAccessService _databaseAccessService;
+        public AccountManagementService(IDatabaseAccessService databaseAccessService)
+        {
+            _databaseAccessService = databaseAccessService;
+        }
+        public void CreateAccount(string email, string password)
+        {
+            if(!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+            {
+                _databaseAccessService.SaveUserInDatabase(email, password);
+                try
+                {
+                    var emilService = new EmailService();
+                    emilService.SendEmailToUser(email);
+                }
+                catch(Exception ex)
+                {
+                    _databaseAccessService.DeleteUserAccount(email);
+                }
+            }
+        }
+    }
+}
---
+++ Asp.NetCoreMvc-Own-Practice-B5/DependencyEx-1/EmailService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyEx_1
+{
+    public class EmailService 
+    {
+        public void SendPasswordRecoveryEmail(string email)
+        {
+
+        }
+        public void SendEmailToUser(string email)
+        {
+
+        }
+
+        public void SendBroadcastEmail(List<string> emails, string subject, string body)
+        {
+
+        }
+    }
+}
